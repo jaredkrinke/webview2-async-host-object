7d6c4987a326c4046dbdddda31392f456442b5d8
Jared Krinke <jared.krinke@gmail.com>
2022-10-30 22:33:41 -0700

    Better type checking

--- ts/proxy-wrapper.ts
+++ ts/proxy-wrapper.ts
@@ -24,30 +24,26 @@ export class ProxyWrapper {
         });
     }
 
-    private dispatch<TResult>(getHostObjectAsync: Promise<{}>, functionName: string, ...args: any[]): Promise<TResult> {
+    private dispatch<TFunction extends (...args: any[]) => Promise<string>, TResult>(f: TFunction, ...args: Parameters<TFunction>): Promise<TResult> {
         return new Promise((resolve, reject) => {
-            getHostObjectAsync
-                .then(hostObject => {
-                    hostObject[functionName](...args)
-                        .then((workItemId: string) => {
-                            this.workItemIdToHandlers[workItemId] = {
-                                resolve: (jsonString: string) => {
-                                    resolve(JSON.parse(jsonString) as TResult);
-                                },
+            f(...args)
+                .then((workItemId: string) => {
+                    this.workItemIdToHandlers[workItemId] = {
+                        resolve: (jsonString: string) => {
+                            resolve(JSON.parse(jsonString) as TResult);
+                        },
 
-                                reject: (errorCodeString: string) => {
-                                    reject(new Error(`Error from work item: ${errorCodeString}`));
-                                }
-                            };
-                        })
-                        .catch(reject);
+                        reject: (errorCodeString: string) => {
+                            reject(new Error(`Error from work item: ${errorCodeString}`));
+                        }
+                    };
                 })
                 .catch(reject);
         });
     }
 
     // UPDATE: Declare JavaScript-friendly proxy methods here
-    public getThreadIdWithDelayAsync(delayMS: number): Promise<string> {
-        return this.dispatch(chrome.webview.hostObjects.example, "startGetThreadIdWithDelayAsync", delayMS);
+    public async getThreadIdWithDelayAsync(delayMS: number): Promise<string> {
+        return this.dispatch((await chrome.webview.hostObjects.example).startGetThreadIdWithDelayAsync, delayMS);
     }
 }
